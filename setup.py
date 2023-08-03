64da2af430973495e54e3e7b4127112251a4a4fe
Ryan Williams <ryan@runsascoded.com>
2023-08-02 20:31:40 -0400

    add find_packages()

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="guss",
     version="0.0.1",
     install_requires=open("requirements.txt").read(),
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'git-update-meta-branch=guss.git_update_meta_branch:main',
